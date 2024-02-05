81be6e6a91fe61c6d5bb2bfb51ffb6587070e817
vahidsh1 <vahidsh1@gmail.com>
2024-02-05 22:58:40 +0330

    adding methods for sum of the matrix and backtrack solution

--- src/main/java/com/exampel/lightsout2/Lightsout.java
+++ src/main/java/com/exampel/lightsout2/Lightsout.java
@@ -3,6 +3,7 @@ package com.exampel.lightsout2;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 
@@ -19,9 +20,15 @@ public class Lightsout {
         String piecesString = reader.readLine();
         String[] piecesArr = piecesString.replace(",", "Y").split(" ");//[..X|XXX|X.., X|X|X, .X|XX, XX.|.X.|.XX, XX|X., XX, .XX|XX.]
         reader.close();
+        long startTime = System.currentTimeMillis() / 1000;
 
         int[][] board = toMatrix(boardArray); // create board
         List<String[][]> listMatrix = toPiecesListMatrix(piecesArr); // create list of pieces
+        System.out.println(solution(board,listMatrix,depth));
+
+
+        System.out.println("# FINISHED AT " + new Date() + "; Elapsed Time: " + (System.currentTimeMillis() / 1000 - startTime) + " Second");
+
 
     }
 
@@ -55,4 +62,78 @@ public class Lightsout {
         return arrayList;
 
     }
+
+
+    private static void placePieceOnBoard(int[][] board, String[][] piece, final int startRow, final int startCol, final int depth) {//OK
+        if (startRow + piece.length > board.length || startCol + piece[0].length > board[0].length) return;
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[0].length; j++) {
+                if (piece[i][j].charAt(0) == 'X') {
+                    board[startRow + i][startCol + j] = ((board[startRow + i][startCol + j] + 1) % depth);
+                }
+            }
+        }
+    }
+
+    public static String  solution(int[][] board, List<String[][]> listMatrix, int depth){
+        StringBuilder result= new StringBuilder();
+        backtrack(board, listMatrix, -1, depth, result);// go to solve
+        return result.reverse().toString().trim();
+    }
+
+    private static boolean backtrack(int[][] board, List<String[][]> listMatrix, int indexListMatrix, int depth, StringBuilder result) {
+
+        if (indexListMatrix >= listMatrix.size() - 1) {//If our pieces are finished. Then return false.
+            return false;
+        }
+        indexListMatrix++;// Select the next piece
+        for (int row = 0; row < board.length; row++) { //traverse the matrix
+            for (int col = 0; col < board[0].length; col++) {
+                if (row + listMatrix.get(indexListMatrix).length > board.length || col + listMatrix.get(indexListMatrix)[0].length > board[0].length)
+                    break;//If piece violated the boarder of the board, go for the next item of board.
+                int[][] bk = copyBoard(board);//for backtrack. Save the current board.
+                placePieceOnBoard(board, listMatrix.get(indexListMatrix), row, col, depth);//Place a piece on the board
+
+                Arrays.stream(board)
+                        .flatMapToInt(x -> Arrays.stream(x))
+                        .forEach(element -> System.out.println(element));
+                System.out.println("new : ");
+
+
+                if (sumBoardMatrix(board) == 0) {//if we found a solution then return true.
+                    if ((indexListMatrix) == (listMatrix.size() - 1)) {//if all pieces are finished and found a solution then return true.
+                        result.append(row).append(",").append(col).append(" ");
+                        return true;
+                    } else return false;
+                }
+                if (!backtrack(board, listMatrix, indexListMatrix, depth, result)) {//call the solution with the next piece.
+                    board = copyBoard(bk);//Revert-> backtrack if the above condition is false
+                } else {
+                    result.append(row).append(",").append(col).append(" ");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int[][] copyBoard(int[][] board) {
+        int[][] bk = new int[board.length][];
+        for (int i = 0; i < board.length; i++)
+            bk[i] = board[i].clone();
+        return bk;
+    }
+
+    private static int sumBoardMatrix(final int[][] board) {
+        int result = 0;
+        for (int[] ints : board) {
+            for (int anInt : ints) {
+                result += anInt;
+            }
+        }
+        return result;
+    }
+
 }
+
+
